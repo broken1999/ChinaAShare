8141b758fb60b7e6cef7e57bb10308227214c84b
broken1999 <137610948@qq.com>
2019-03-04 23:04:19 +0100

    main.py get daily quotation

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pandas as pd
 from pandas import Series, DataFrame
 import tushare as ts
 import matplotlib.pyplot as plt
+import datetime
+
+#ts.set_token('09f77414f088aad7959f5eecba391fe685ea50462e208ce451b1b6a6')
+tspro = ts.pro_api('09f77414f088aad7959f5eecba391fe685ea50462e208ce451b1b6a6')
+StockBasic = tspro.query('stock_basic', list_status='L')
+HighPoint2015 = pd.read_pickle('retrieve_2015_data/HighPoint2015010120151231.pkl')
 
 
 def tbt(ctb, lowlim=0, upplim=1):
@@ -28,21 +34,26 @@ refresh = 1
 
 if refresh:
     ## today quotation
-    td = ts.get_today_all()
-    td.to_pickle('td.pkl')
+    #td = ts.get_today_all()
+    todaydate=datetime.datetime.today().strftime('%Y%m%d')
+    td = tspro.daily_basic(trade_date=todaydate)
+    td.to_pickle('td'+todaydate+'.pkl')
     ## performance
-    yj = ts.get_report_data(2017, 3)
-    yj.to_pickle('yj.pkl')
+    #yj = ts.get_report_data(2017, 3)
+    #yj.to_pickle('yj.pkl')
     ## basics
-    basics = ts.get_stock_basics()
-    basics.to_pickle('basics.pkl')
+    #basics = ts.get_stock_basics()
+    #basics.to_pickle('basics.pkl')
 if (1 - refresh):
-    td = pd.read_pickle('td.pkl')
-    yj = pd.read_pickle('yj.pkl')
-    basics = pd.read_pickle('basics.pkl')
+    td = pd.read_pickle('td'+todaydate+'.pkl')
+    #yj = pd.read_pickle('yj.pkl')
+    #basics = pd.read_pickle('basics.pkl')
+
+#### below needs revision
+
 
 ## construct dataframe code, trade, bvps
-trade = td[['code', 'trade']].set_index('code')
+trade = td[['ts_code', 'close']]
 bvps = basics['bvps'].to_frame()
 name = basics[['name', 'industry']]  # .to_frame()
 #name.drop_duplicates(keep=False,inplace=True)
