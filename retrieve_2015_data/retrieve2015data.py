b983d616e316a276e733055814022169c36a35aa
broken1999 <137610948@qq.com>
2019-03-11 22:49:22 +0100

    [bug] 2015 high point is not qfq

--- retrieve_2015_data/retrieve2015data.py
+++ retrieve_2015_data/retrieve2015data.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 import tushare as ts
+import datetime
 
 #ts.set_token('09f77414f088aad7959f5eecba391fe685ea50462e208ce451b1b6a6')
 pro = ts.pro_api('09f77414f088aad7959f5eecba391fe685ea50462e208ce451b1b6a6')
@@ -15,15 +16,25 @@ stockcodepool = StockBasic[(StockBasic['list_date']<'20151231') & (StockBasic['m
 Time_rangemin='20150101'
 Time_rangemax='20151231'
 
+# date of today and adjustment factors
+todaydate=datetime.datetime.today().strftime('%Y%m%d')
+#AdjustmentFactorToday = pro.adj_factor(ts_code='', trade_date=todaydate)
+#AdjustmentFactorTimeRangeMax = pro.adj_factor(ts_code='', trade_date=Time_rangemax)
+
 HighPoint2015 = pd.DataFrame(columns=['ts_code', 'HighPoint2015'])
 for ts_code in stockcodepool['ts_code']:
     print(ts_code)
     # 未复权
     # temp = pro.monthly(ts_code=ts_code, start_date=Time_rangemin, end_date=Time_rangemax,fields='ts_code, high')
-    # 前复权， 当日收盘价 × 当日复权因子 / 最新复权因子
     try:
-        temp = ts.pro_bar(pro_api=pro, ts_code=ts_code, freq='M', adj='qfq', start_date=Time_rangemin, end_date=Time_rangemax )
-        HighPoint2015 = HighPoint2015.append(pd.DataFrame({'ts_code':[ts_code], 'HighPoint2015': [temp['high'].max()]}), ignore_index=True)
+        # 伪前复权， 当日收盘价 × 当日复权因子 / 20151231复权因子
+        # 前复权， 当日收盘价 × 当日复权因子 / 20151231复权因子 × (20151231复权因子 / 最新复权因子)
+        # 这里使用不复权价格，配合复权因子
+        quote = ts.pro_bar(pro_api=pro, ts_code=ts_code, freq='D', adj=None , start_date=Time_rangemin, end_date=Time_rangemax )
+        HighPoint = quote['high'].max()
+        HighPointDate = quote.at[quote[quote['high']== HighPoint].index[0],'trade_date']
+        AdjustmentFactorHighPointDate = pro.adj_factor(ts_code=ts_code, trade_date=HighPointDate)
+        HighPoint2015 = HighPoint2015.append(pd.DataFrame({'ts_code':[ts_code], 'HighPoint2015': [HighPoint], 'AdjustmentFactorHighPointDate':[AdjustmentFactorHighPointDate.at[0,'adj_factor']]}), ignore_index=True, sort=True)
     except TypeError:
         continue
 
